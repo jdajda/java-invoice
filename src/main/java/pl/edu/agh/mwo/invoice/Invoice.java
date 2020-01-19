b74393e3e6e1c1728f4260822104fd5c4c871a19
MJ <michaljalowik@gmail.com>
2020-01-19 15:57:28 +0100

    zrobilem 5 testow jak narazie

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,29 +2,41 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+
+	private Map<Product, Integer> products = new HashMap<>();
+
+
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		this.addProduct(product,1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		this.products.put(product, quantity);
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getNetSum() {
+
+		BigDecimal sum = BigDecimal.ZERO;
+
+		for(Product product : this.products.keySet()){
+			Integer quantity = this.products.get(product);
+			sum = sum.add(product.getPrice().multiply(new BigDecimal(quantity)));
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		return BigDecimal.ZERO;
 	}
 
-	public BigDecimal getTotal() {
-		return null;
+	public BigDecimal getGrossSum() {
+		return BigDecimal.ZERO;
 	}
 }
